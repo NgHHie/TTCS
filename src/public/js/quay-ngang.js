58103ed5e491dd07bf4c66217215e980d9109fa1
CoVoCuc <TriDX.B21CN721@stu.ptit.edu.vn>
2024-05-31 18:55:25 +0700

    u final

--- src/public/js/quay-ngang.js
+++ src/public/js/quay-ngang.js
@@ -1,13 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
     function checkOrientation() {
-        if (window.innerHeight > window.innerWidth) {
-            alert("Vui lòng quay ngang màn hình để có trải nghiệm tốt nhất.")
+        if (window.innerWidth < 1024 && window.innerWidth < window.innerHeight) {
+            // alert("Vui lòng quay ngang màn hình để có trải nghiệm tốt nhất.")
+            if (!sessionStorage.getItem('alertShown')) {
+                alert('Vui lòng quay ngang màn hình để có trải nghiệm tốt nhất.');
+                sessionStorage.setItem('alertShown', 'true');
+            }
         }
-    }
-    
+    } 
     checkOrientation();
-
-    window.addEventListener('resize', function() {
-        checkOrientation();
-    });
 });

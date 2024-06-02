e428a1cfa58a590b4394e022dbd422dd3f88dd3c
huongka234 <nguyenquanghuong911@gmail.com>
2024-06-02 17:08:56 +0700

    test

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config()
-const sequelize = new Sequelize("javasql2", "root", "1111", {
+const sequelize = new Sequelize("fashion-app11", "root", "123456789", {
   host: "localhost",
-  port: "3307",
+  port: "3306",
   dialect: "mysql",
 });
 const connection = async () => {

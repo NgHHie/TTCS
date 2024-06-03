9d74560a7e3629f9b15980922e614d721aa8b4df
Nguyễn Hoàng Hiệp <hiep2003ka@gmail.com>
2024-06-03 18:15:31 +0700

    Update question.js

--- src/models/question.js
+++ src/models/question.js
@@ -33,7 +33,7 @@ module.exports = (sequelize, DataTypes) => {
             // options
             sequelize,
             modelName: 'Question',
-            tableName: 'CauHoi',
+            tableName: 'cauhoi',
             timestamps: false,
 
         },
